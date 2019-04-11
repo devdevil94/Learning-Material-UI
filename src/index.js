3097d52423fc44d8be5b8c591e4177358ae27ab7
Saud Al Alawi <41317640+devdevil94@users.noreply.github.com>
2019-04-11 13:30:07 +0400

    Components added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import NavBar from "./components/NavBar";
 
 import "./styles.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 const rootElement = document.getElementById("root");
